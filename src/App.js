686369b049e410dec9908e2aa6d25c1394ea58a2
IkramSenoussi <75143996+IkramSenoussi@users.noreply.github.com>
2022-01-25 21:47:58 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,35 +8,63 @@ import Map from './components/Map/Map';
 
 const App = () => {
     const [places, setPlaces] = useState([]);
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
 
-    const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
-    const [bounds, setBounds] = useState(null);
+    const [childClicked, setChildClicked] = useState(null);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState({});
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
+    
+    useEffect(() => {
+      navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      })
+    }, []);
+
+    useEffect(() => {
+        const filteredPlaces = places.filter((place) => place.rating > rating);
+
+        setFilteredPlaces(filteredPlaces);
+    }, [rating]);
 
   useEffect(() => {
-        console.log(coordinates, bounds);
+        setIsLoading(true);
 
-        getPlacesData()
+        getPlacesData(type, bounds.sw, bounds.ne)
             .then((data) => {
-                 console.log(data);
-                 
-
                  setPlaces(data);
+                 setFilteredPlaces([])
+                 setIsLoading(false);
             })
-  }, [coordinates, bounds]);
+  }, [type, coordinates, bounds]);
 
     return(
         <>
         <CssBaseline />
-      <Header />
+      <Header setCoordinates={setCoordinates} />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List/>
+          <List
+              places={filteredPlaces.length ? filteredPlaces : places}
+              childClicked={childClicked}
+              isLoading={isLoading}
+              type={type}
+              setType={setType}
+              rating={rating}
+              setRating={setRating}
+           />
        </Grid>
         <Grid item xs={12} md={8}>
           <Map
-              setCoordinates={ setCoordinates}
-              setBounds={ setBounds}
+              setCoordinates={setCoordinates}
+              setBounds={setBounds}
               coordinates={coordinates}
+              places={filteredPlaces.length ? filteredPlaces: places}
+              setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
-
-const options = {
-    params: {
-      bl_latitude: '11.847676',
-      tr_latitude: '12.838442',
-      bl_longitude: '109.095887',
-      tr_longitude: '109.149359',
-    },
-    headers: {
-      'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-      'x-rapidapi-key': '3387daac1emsh18186dfac3ab6b0p1189f2jsnd8b014e08a8f'
-    }
-  };
-export const getPlacesData = async () => {
+export const getPlacesData = async (type, sw, ne) => {
     try {
-        const { data: { data } } = await axios.get(URL, options);
+        const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+            'x-rapidapi-key': '02abd1d46emsh453ead362e5732ep11ecd1jsnb41fc4413ba0'
+          }
+        });
 
         return data;
-    } catch (error){
+    } catch (error) {
         console.log(error)
     }
 }
