c26232605c97332cb6b42f97d0fdfacb70562209
IkramSenoussi <75143996+IkramSenoussi@users.noreply.github.com>
2022-01-26 23:54:55 +0100

    Add files via upload

---
+++ landing page react/src/static/places.js
@@ -0,0 +1,18 @@
+const places = [
+  {
+    title: 'new parc ',
+    description:
+      "The new parc is home to some of the world's most ravishing green place, but it's the animals, which truly makes this parc shine.The Green Park is a peaceful triangle of mature trees and grasslands and offers a quiet retreat from city life, right next to Buckingham Palace.",
+    imageUrl: process.env.PUBLIC_URL + '/assets/island1.jpg',
+    time: 1500,
+  },
+  {
+    title: 'Mogador',
+    description:
+      'Shaped like a giant sombrero, this lush volcanic island stars in countless South Atlantic Ocean. The focal point and best asset of this tropical beauty is its ravishing lagoon in technicolor turquoise. Fish, turtles, sharks, and rays swim in the clear waters.',
+    imageUrl: process.env.PUBLIC_URL + '/assets/island2.jpg',
+    time: 1500,
+  },
+];
+
+export default places;
